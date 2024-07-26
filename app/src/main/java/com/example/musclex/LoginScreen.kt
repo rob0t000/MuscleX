4dc29527f7cd6f12b55d5118388ff2f8545eb53c
rob0t000 <hamzagst3@gmail.com>
2024-07-26 16:47:09 +0530

    My new project for S.I.E.S GST Tech Team

---
+++ app/src/main/java/com/example/musclex/LoginScreen.kt
@@ -0,0 +1,240 @@
+package com.example.musclex
+
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import kotlinx.coroutines.delay
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    val gradient = Brush.verticalGradient(
+        colors = listOf(
+            colorResource(id = R.color.gradient_start_color),
+            colorResource(id = R.color.gradient_middle_color),
+            colorResource(id = R.color.gradient_end_color)
+        )
+    )
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    LaunchedEffect(password) {
+        if (password.isNotEmpty()) {
+            isPasswordVisible = true
+            delay(200)
+            isPasswordVisible = false
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = gradient)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logoapp),
+                contentDescription = "Login image",
+                modifier = Modifier
+                    .size(250.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Text(
+                text = "MUSCLEX",
+                style = TextStyle(
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    fontFamily = FontFamily.Serif
+                ),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Welcome Builder",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Login into your Account",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    color = Color.White
+                ),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .border(BorderStroke(2.dp, Color.Transparent), RoundedCornerShape(8.dp))
+                    .padding(16.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text(text = "Email address") },
+                        textStyle = TextStyle(color = Color.White),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFF0F2027),
+                            unfocusedBorderColor = Color(0xFF0F2027),
+                            cursorColor = Color(0xFF0F2027),
+                            focusedLabelColor = Color(0xFFA91818),
+                            unfocusedLabelColor = Color.Gray
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text(text = "Password") },
+                        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        textStyle = TextStyle(color = Color.White),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Color(0xFFA91818),
+                            unfocusedBorderColor = Color(0xFFA91818),
+                            cursorColor = Color(0xFFA91818),
+                            focusedLabelColor = Color(0xFFA91818),
+                            unfocusedLabelColor = Color.Gray
+                        ),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = {
+
+                            Log.d("LoginScreen", "User logged in with email: $email")
+                            Log.i("Creds", "Email: $email, Password: $password")
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFA91818) // Fill with #A91818 color
+                        ),
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(
+                            text = "Login",
+                            color = Color.White
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Forgot password",
+                modifier = Modifier
+                    .clickable {  }
+                    .align(Alignment.CenterHorizontally),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Or sign in with",
+                modifier = Modifier
+                    .clickable {  }
+                    .align(Alignment.CenterHorizontally),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    color = Color.White
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 40.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.twitter),
+                    contentDescription = "Twitter",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .weight(1f)
+                        .padding(16.dp)
+                        .clickable {  }
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.google),
+                    contentDescription = "Google",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .weight(1f)
+                        .padding(16.dp)
+                        .clickable {  }
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.facebook),
+                    contentDescription = "Facebook",
+                    modifier = Modifier
+                        .size(70.dp)
+                        .weight(1f)
+                        .padding(16.dp)
+                        .clickable {  }
+                )
+            }
+        }
+    }
+}
